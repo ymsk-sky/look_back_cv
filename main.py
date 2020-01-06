91460b07e46d67ec5b3f31dbcad52ef5f2413191
ymsk.sky <ymsk.sky.95@gmail.com>
2020-01-06 22:38:15 +0900

    説明コメント追加

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+透過画像の合成は公式チュートリアルを参考
+https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_core/py_image_arithmetics/py_image_arithmetics.html#bitwise-operations
+"""
 
 import numpy as np
 import cv2
 
+# カメラ番号
 CAMERA = 2
 
 # 顔検出カスケードファイル読み込み
 cascade_path = "/usr/local/var/pyenv/versions/3.7.1/lib/python3.7/site-packages/cv2/data/"
 cascade_file = "haarcascade_frontalface_default.xml"
 cascade = cv2.CascadeClassifier(cascade_path+cascade_file)
+
 
 def main(base_width, base_height):
     # カメラ読み込み
@@ -17,8 +23,11 @@ def main(base_width, base_height):
     # 合成画像読み込み
     img = cv2.imread('obake_resized.png')
 
+    # 合成する画像情報を取得
     rows, cols, channels = img.shape
+    # グレースケール
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # マスク作成
     ret, mask = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
     mask_inv = cv2.bitwise_not(mask)
 
@@ -42,7 +51,10 @@ def main(base_width, base_height):
 
             # キャリブレーションしたサイズと同程度の場合に顔と判定
             if(rect[2] > base_width*0.8 or rect[3] > base_height*0.8):
+                # 顔認識確認用
                 # cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 0, 0), thickness=4)
+
+                # 顔検出時（正面を向いているとき）におばけ出現 <-> 振り向くと消える
                 appear_obake(frame, img, rows, cols, mask, mask_inv)
 
         # 表示
@@ -57,7 +69,9 @@ def main(base_width, base_height):
 
 
 def appear_obake(frame, img, rows, cols, mask, mask_inv):
+    # 合成する位置を決める
     roi = frame[0:rows, 0:cols]
+
     frame_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
     img_fg = cv2.bitwise_and(img, img, mask=mask)
 
