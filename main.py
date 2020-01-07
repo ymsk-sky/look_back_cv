9b5c0633fe5c6a4d18b4ee73667429befad054da
ymsk.sky <ymsk.sky.95@gmail.com>
2020-01-07 22:00:28 +0900

    キャリブレーション動作をメインに実装

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ cascade_file = "haarcascade_frontalface_default.xml"
 cascade = cv2.CascadeClassifier(cascade_path+cascade_file)
 
 
-def main(base_width, base_height):
+def main():
     # カメラ読み込み
     cap = cv2.VideoCapture(CAMERA)
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -31,6 +31,11 @@ def main(base_width, base_height):
     ret, mask = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
     mask_inv = cv2.bitwise_not(mask)
 
+    # 顔判定の元となるサイズ
+    base_width = 0
+    base_height = 0
+
+    calibration_flag = False
     cnt = 0
     while(1):
         ret, frame = cap.read()
@@ -42,7 +47,8 @@ def main(base_width, base_height):
             # グレースケール
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # すべての顔（らしきところを）認識
-            facerect = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+            facerect = cascade.detectMultiScale(gray, scaleFactor=1.1,
+                                                minNeighbors=1, minSize=(1, 1))
 
         # ひとつでも検出されている場合
         if(len(facerect) > 0):
@@ -51,14 +57,22 @@ def main(base_width, base_height):
 
             # キャリブレーションしたサイズと同程度の場合に顔と判定
             if(rect[2] > base_width*0.8 or rect[3] > base_height*0.8):
-                # 顔認識確認用
-                # cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 0, 0), thickness=4)
+                if(calibration_flag):
+                    cv2.rectangle(frame, tuple(rect[0:2]),
+                                         tuple(rect[0:2]+rect[2:4]),
+                                         (255, 0, 0), thickness=4)
 
                 # 顔検出時（正面を向いているとき）におばけ出現 <-> 振り向くと消える
                 appear_obake(frame, img, rows, cols, mask, mask_inv)
 
         # 表示
         cv2.imshow('cap', frame)
+
+        if(cv2.waitKey(1) & 0xFF == ord('o')):
+            if(calibration_flag):
+                base_width = rect[2]
+                base_height = rect[3]
+            calibration_flag = not calibration_flag
 
         if(cv2.waitKey(1) & 0xFF == ord('q')):
             break
@@ -79,31 +93,5 @@ def appear_obake(frame, img, rows, cols, mask, mask_inv):
     frame[0:rows, 0:cols] = dst
 
 
-def calibration():
-    cap = cv2.VideoCapture(CAMERA)
-    color = (0, 0, 255)
-    while(1):
-        ret, frame = cap.read()
-        if(not ret):
-            break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        facerect = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-
-        if(len(facerect) > 0):
-            rect = facerect[np.argmax([r[2] for r in facerect])]
-            cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), color, thickness=4)
-
-        cv2.imshow('cap', frame)
-
-        if(cv2.waitKey(1) & 0xFF == ord('o')):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return rect[2], rect[3]
-
-
 if __name__ == '__main__':
-    base_width, base_height = calibration()
-    main(base_width, base_height)
+    main()
