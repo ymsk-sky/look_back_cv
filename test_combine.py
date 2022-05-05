bc63ab5d28553cdff9c4fb0429b2d607267901b3
ymsk-sky <ymsk.sky.95@gmail.com>
2022-05-05 19:48:31 +0900

    Test to implement a-val recalculate

--- test_combine.py
+++ test_combine.py
@@ -5,12 +5,36 @@ IMAGE_FILE ="./obake_resized.png"
 BACK_FILE ="./background.jpg"
 G_FILE ="./gradation.jpg"
 
-img = cv2.imread(IMAGE_FILE)
+img = cv2.imread(IMAGE_FILE, -1)  # アルファ値を含んで読み込む
 background = cv2.imread(BACK_FILE)
 g = cv2.imread(G_FILE)
-g2 = cv2.resize(g, img.shape[1::-1])
-g2 = cv2.bitwise_not(g2)
+g2 = cv2.resize(g, img.shape[1::-1])  # リサイズ
+g3 = cv2.cvtColor(g2, cv2.COLOR_BGR2GRAY)  # グレースケール
+g3 = cv2.bitwise_not(g3)  # 反転
 
+rows, cols, _ = img.shape
+
+img2 = img.copy()
+for i in range(rows):
+    for j in range(cols):
+        if(img[i][j][3] == 0):
+            pass
+        else:
+            img2[i][j][3] = g3[i][j]
+
+cv2.imwrite("img2.png", img2)
+
+"""
+[
+    [[R,G,B,A],...,[R,G,B,A]],
+    [[R,G,B,A],...,[R,G,B,A]],
+    ...
+    [[R,G,B,A],...,[R,G,B,A]],
+    [[R,G,B,A],...,[R,G,B,A]]
+]
+"""
+
+"""
 rows, cols, _ = img.shape
 w, h, _ = background.shape
 
@@ -27,3 +51,4 @@ dst = cv2.add(background_bg, img_fg)
 background[0:rows, 0:cols] = dst
 
 cv2.imwrite("test.jpg", background)
+"""
